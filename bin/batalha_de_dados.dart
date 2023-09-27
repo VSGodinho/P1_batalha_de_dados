07eac1536961eb618e35051bf35062e898eadd2b
Vinicius Godinho <v.godinhu@hotmail.com>
2023-09-27 04:24:04 -0300

    BDD: Apresentação e rodadas adicionados

---
+++ bin/batalha_de_dados.dart
@@ -0,0 +1,5 @@
+import 'package:batalha_de_dados/batalha_de_dados.dart' as batalha_de_dados;
+
+void main(List<String> arguments) {
+  jogo();
+}
---
+++ lib/batalha_de_dados.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'dart:math';
+
+void jogo() {
+  //Introdução e Rodadas
+  stdout.writeln("Batalha de Dados\nQuantas rodadas gostaria de jogar?");
+  int n = int.parse(stdin.readLineSync()!);
+  //Rolagem, Calculo e Determinação de vencedor
+
+  //Placar e vencedor
+}
