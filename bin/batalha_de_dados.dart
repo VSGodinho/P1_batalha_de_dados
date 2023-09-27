3d16c2fdcc1e507656c2c2bdcd5da3fbfb513635
Vinicius Godinho <v.godinhu@hotmail.com>
2023-09-27 11:15:13 -0300

    BDD: Correções

--- bin/batalha_de_dados.dart
+++ bin/batalha_de_dados.dart
@@ -5,7 +5,7 @@ void main() {
   int v = 0, p = 0, e = 0;
   //Introdução e Rodadas
   stdout.writeln("Batalha de Dados\nQuantas rodadas gostaria de jogar?");
-  int n = int.parse(stdin.readLineSync()!);
+  int? n = int.parse(stdin.readLineSync()!);
 
   //Rolagem, Calculo e Determinação de vencedor
   for (int i = 0; i < n; i++) {
@@ -26,21 +26,21 @@ void main() {
 
     //Vencedor
     if (sd1 > sd2) {
-      stdout.writeln("Você ganhou a rodada!!");
+      stdout.writeln("Você ganhou a rodada!!\n");
       v = v + 1;
     } else {
       if (sd2 > sd1) {
-        stdout.writeln("Você perdeu a rodada!!");
+        stdout.writeln("Você perdeu a rodada!!\n");
         p = p + 1;
       } else {
-        stdout.writeln("Empate!!");
+        stdout.writeln("Empate!!\n");
         e = e + 1;
       }
     }
   }
 
   //Placar e vencedor
-  stdout.writeln("Resultado Final:\nVitorias: $v\nDerrotas: $p\nEmpates: $e");
+  stdout.writeln("Resultado Final:\nVitorias: $v\nDerrotas: $p\nEmpates: $e\n");
   if (v > p) {
     stdout.writeln("Você ganhou a partida!!");
   } else {
